50713b9f1654e653951f3744fb0b37c68fe96a56
Luis Laureano <lcanario@sj.com.do>
2019-12-02 17:00:29 -0400

    .

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-aapp.use(express.static(__dirname + '/dist/VerifyIt'));
+aapp.use(express.static(__dirname + '/dist/argon-dashboard-angular'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/VerifyIt/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/argon-dashboard-angular/index.html'));
 
 });
 
