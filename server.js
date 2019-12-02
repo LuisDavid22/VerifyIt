22f974116efa09cc0f588b779b01b3b5b652febf
Luis Laureano <lcanario@sj.com.do>
2019-12-02 15:49:52 -0400

    asd

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+aapp.use(express.static(__dirname + '/dist/argon-dashboard-angular'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*',function(req,res){
-    res.sendfile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/argon-dashboard-angular/index.html'));
 
 })
 
