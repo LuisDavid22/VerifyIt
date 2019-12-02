0425417743bc09842c061375fa3c6631ff006fa4
Luis Laureano <lcanario@sj.com.do>
2019-12-02 16:16:37 -0400

    adsf

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-aapp.use(express.static(__dirname + '/dist/argon-dashboard-angular'));
+aapp.use(express.static(__dirname + '/dist/VerifyIt'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/argon-dashboard-angular/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/VerifyIt/index.html'));
 
-})
+});
 
 console.log('Console Listening!');
