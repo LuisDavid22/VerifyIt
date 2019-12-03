207f8537fd231aa5e83b1b1f3558b96d7e910dc8
Luis Laureano <lcanario@sj.com.do>
2019-12-03 15:36:24 -0400

    asa

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const path = require('path');
 
@@ -11,4 +11,22 @@ app.get('/*',function(req,res){
 
 });
 
-console.log('Console Listening!');
+console.log('Console Listening!');*/
+
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/argon-dashboard-angular'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/argon-dashboard-angular/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
